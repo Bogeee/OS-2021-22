1a236c5a9763e4f7bc0df527d025429b108c18e1
Roberto Carletto <roberto.carletto@edu.unito.it>
2022-10-04 15:45:20 +0200

    user.c signal handler

--- src/user.c
+++ src/user.c
@@ -14,18 +14,15 @@ transaction (*shmLibroMastroMatrix)[SO_REGISTRY_SIZE]; /* ID shmem Array of tran
 #endif
 
 void shutdown(int);
+int getBilancio();
+void createTransaction();
+void signalHandler();
 
 int main(int argc, char ** argv){
 
-    int k = 0, j = 0;
     int fails = 0; /* used with SO_RETRY */
     int bilancio; /*  */
     int status = 0; /*  */
-    transaction newTr; /* new transaction */
-    int randomReceiver; /* Random user */
-    int randomNode; /* Random node */
-    int randomQuantity; /* Random quantity for the transaction */
-    int nodeReward; /* Transaction reward */
 
     /* Creating the shared memory segment */
     shmConfig = shmget(SHM_ENV_KEY, 0, 
@@ -36,7 +33,7 @@ int main(int argc, char ** argv){
 		shutdown(EXIT_FAILURE);
 	}
 	
-    conf = shmat(shmConfig, NULL, 0);
+    conf = shmat(shmConfig, NULL, SHM_RDONLY);
 
     semUsers = semget(SEM_USER_KEY, 3, 0666);
     if(semUsers == -1){
@@ -67,55 +64,18 @@ int main(int argc, char ** argv){
 		shutdown(EXIT_FAILURE);
 	}
 
-
-    bilancio = conf[SO_BUDGET_INIT];
+    if (signal(SIGUSR1, signalHandler) == SIG_ERR)  {
+		MSG_ERR("main: signalHandler, error.");
+        perror("signal SIGUSR1 error");
+        shutdown(EXIT_FAILURE);
+    }
 
     /* Starting user loop */
     while(fails < conf[SO_RETRY]) {
-        /* TODO: controllo transazioni fallite */
-        /* calcolo bilancio - lettura with and readers solution */
-
-        initReadFromShm(semLibroMastro);
-        shmLibroMastroMatrix = shmat(shmLibroMastro, NULL, 0);
-        if(shmLibroMastroMatrix == (void *) -1)
-            perror("shmLibroMastroMatrix shmat ");
-        for (k = 0; k < SO_REGISTRY_SIZE; k++)
-        {
-            for (j = 0; j < SO_BLOCK_SIZE; j++)
-            {
-                /* TODO: calcolo bilancio dev'essere calcolato anche in base alle transazioni in coda
-                        contare anche i reward inviati ai nodi */
-                /* printf("LibroMastro[%d][%d] vale %d\n", k, j, shmLibroMastroMatrix[k][j].quantity); */
-
-                if(shmLibroMastroMatrix[k][j].receiver == getpid())
-                    bilancio += shmLibroMastroMatrix[k][j].quantity;
-
-                if(shmLibroMastroMatrix[k][j].sender == getpid())
-                    bilancio -= shmLibroMastroMatrix[k][j].quantity;
-            }
-            
-        }
-        shmdt((void *)shmLibroMastroMatrix);
-        if(shmLibroMastroMatrix == (void *) -1)
-            perror("shmLibroMastroMatrix shmdt ");
-        endReadFromShm(semLibroMastro);
-        
-        printf("[%d] ha bilancio uguale a : %d\n", getpid(), bilancio);
+        bilancio = getBilancio();
         if(bilancio >= 2) 
         {
-            shutdown(EXIT_SUCCESS);
-            /* TODO: genera transazione */
-            randomReceiver = randomNum(0, conf[SO_USERS_NUM]-1);
-            randomNode = randomNum(0, conf[SO_NODES_NUM]-1);
-            randomQuantity = randomNum(2, bilancio);
-            nodeReward = randomQuantity * conf[SO_REWARD] / 100;
-            randomQuantity -= nodeReward;
-            newTr.quantity = randomQuantity;
-            newTr.receiver = randomReceiver;
-            newTr.reward = nodeReward;
-            newTr.sender = getpid();
-            newTr.timestamp = (int)time(NULL);
-            
+            createTransaction(bilancio);
         } else {
             fails++;
         }
@@ -138,4 +98,70 @@ void shutdown(int status)
 
 	/*msgctl(msgTransactions, IPC_RMID, NULL);*/
 	exit(status);
+}
+
+void createTransaction(int bilancio) {
+
+    transaction newTr; /* new transaction */
+    int randomReceiver; /* Random user */
+    int randomNode; /* Random node */
+    int randomQuantity; /* Random quantity for the transaction */
+    int nodeReward; /* Transaction reward */
+
+    /*shutdown(EXIT_SUCCESS);*/
+    /* TODO: genera transazione */
+    randomReceiver = randomNum(0, conf[SO_USERS_NUM]-1);
+    randomNode = randomNum(0, conf[SO_NODES_NUM]-1);
+    randomQuantity = randomNum(2, bilancio);
+    nodeReward = randomQuantity * conf[SO_REWARD] / 100;
+    randomQuantity -= nodeReward;
+    newTr.quantity = randomQuantity;
+    newTr.receiver = randomReceiver;
+    newTr.reward = nodeReward;
+    newTr.sender = getpid();
+    newTr.timestamp = (int)time(NULL);
+    
+    /* TODO : Invia al nodo = carica nel msgQueue del nodo */ 
+}
+
+int getBilancio() {
+    /* TODO: controllo transazioni fallite */
+    /* calcolo bilancio - lettura with and readers solution */
+    int bilancio = conf[SO_BUDGET_INIT];
+    int k = 0, j = 0;
+    initReadFromShm(semLibroMastro);
+    shmLibroMastroMatrix = shmat(shmLibroMastro, NULL, 0);
+    if(shmLibroMastroMatrix == (void *) -1)
+        perror("shmLibroMastroMatrix shmat ");
+    for (k = 0; k < SO_REGISTRY_SIZE; k++)
+    {
+        for (j = 0; j < SO_BLOCK_SIZE; j++)
+        {
+            /* TODO: calcolo bilancio dev'essere calcolato anche in base alle transazioni in coda
+                    contare anche i reward inviati ai nodi */
+            /* printf("LibroMastro[%d][%d] vale %d\n", k, j, shmLibroMastroMatrix[k][j].quantity); */
+
+            if(shmLibroMastroMatrix[k][j].receiver == getpid())
+                bilancio += shmLibroMastroMatrix[k][j].quantity;
+
+            if(shmLibroMastroMatrix[k][j].sender == getpid())
+                bilancio -= shmLibroMastroMatrix[k][j].quantity;
+        }
+        
+    }
+    shmdt((void *)shmLibroMastroMatrix);
+    if(shmLibroMastroMatrix == (void *) -1)
+        perror("shmLibroMastroMatrix shmdt ");
+    endReadFromShm(semLibroMastro);
+    
+    /*printf("[%d] ha bilancio uguale a : %d\n", getpid(), bilancio);*/
+
+    return bilancio;
+}
+
+void signalHandler() {
+    int bilancio = getBilancio();
+    printf("Segnale ricevuto - creazione transazione\n");
+    if(bilancio > 0)
+        createTransaction(bilancio);
 }
